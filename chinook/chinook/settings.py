cfb40ce5e5757f832d11a62b0a3b58cd32220061
pilargg <63071055+pilargg@users.noreply.github.com>
2022-04-15 20:27:06 +0200

    creation des models

--- chinook/chinook/settings.py
+++ chinook/chinook/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'disks.apps.DisksConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -76,7 +77,7 @@ WSGI_APPLICATION = 'chinook.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': BASE_DIR / 'chinook.db',
     }
 }
 
--- chinook/disks/models.py
+++ chinook/disks/models.py
@@ -1,3 +1,36 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Track(models.Model):
+    
+    name = models.CharField(max_length=200)
+    composer = models.CharField(max_length=200)
+    milliseconds = models.IntegerField(default=0)
+    bytes = models.IntegerField(default=0)
+    unitPrice = models.FloatField(default=0)
+    
+    # associations
+    album = models.ForeignKey("Album", on_delete=models.CASCADE)
+    
+    def __str__(self):
+        return self.name
+    
+    
+class Album(models.Model):
+    
+    title = models.CharField(max_length=200)
+    artist = models.ForeignKey("Artist", on_delete=models.CASCADE)
+    
+    def __str__(self):
+        return self.title
+     
+    
+class Artist(models.Model):
+    
+    name = models.CharField(max_length=200)
+    
+    def __str__(self):
+        return self.name
+    
